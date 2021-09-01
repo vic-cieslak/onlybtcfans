7fd5533ed18d48c9be4a7756d4a3ee0551851d1d
Wiktor Cieślak <wiktorcie@gmail.com>
2021-09-01 17:37:03 +0200

    add icons to notifications page

--- src/pages/Notifications.vue
+++ src/pages/Notifications.vue
@@ -1,7 +1,46 @@
 <template>
-  <q-page padding>
+  <q-page>
     <!-- content -->
-    notifications
+    <div class="q-pa-md q-gutter-md">
+
+     <q-btn
+       flat
+       label='all'
+       color="grey-7"
+       icon="eva-done-all-outline" />
+
+     <q-btn
+       flat
+       label='interactions'
+       color="grey-7"
+       icon="eva-message-circle-outline" />
+
+     <q-btn
+       flat
+       label='liked'
+       color="grey-7"
+       icon="eva-heart-outline" />
+
+     <q-btn
+       flat
+       label='subscribed'
+       color="grey-7"
+       icon="eva-unlock-outline" />
+
+     <q-btn
+       flat
+       label='tipped'
+       color="grey-7"
+       icon="monetization_on" />
+
+     <q-btn
+       flat
+       label='promotions'
+       color="grey-7"
+       icon="eva-alert-triangle-outline" />
+
+    </div>
+    <q-separator />
   </q-page>
 </template>
 
