b12d057b648f38464d15da793b02ed06b6ecc01b
Wiktor Cieślak <wiktorcie@gmail.com>
2021-09-03 22:02:04 +0200

    cool photo to feed

--- src/pages/Feed.vue
+++ src/pages/Feed.vue
@@ -1,6 +1,11 @@
 <template>
-  <q-page padding>
-      FEEEED
+  <q-page>
+      <div>
+
+        <q-img height='300px' width='100%' src="https://firebasestorage.googleapis.com/v0/b/casting-164e8.appspot.com/o/vg5CN9Oj5CVSrjcA6QahhV9tBOq1%2FprofilePhoto%2Fphoto.jpeg?alt=media&token=59521673-7f33-4b80-b0cb-9c06fd854926"></q-img>
+
+
+      </div>
   </q-page>
 </template>
 
