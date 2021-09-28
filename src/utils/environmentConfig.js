c56321d593fcae6b50a42a1446ee1acda18692e1
Wiktor Cieślak <wiktorcie@gmail.com>
2021-09-28 11:17:13 +0200

    update env loader for ci

--- src/utils/environmentConfig.js
+++ src/utils/environmentConfig.js
@@ -1,4 +1,4 @@
-const ENV = require('dotenv').config().parsed
+// const ENV = require('dotenv').config().parsed
 /*
   Use an environment variable set in package.json scripts to determine
   the applications runtime environment. Add more switch cases as
@@ -8,18 +8,19 @@ const ENV = require('dotenv').config().parsed
 
 module.exports = (QENV) => {
   if (!['DEV', 'STAGE', 'PROD'].includes(QENV)) {
-    throw Error('Unknonw or not supplied environment variable')
+    throw Error('Unknown or not supplied environment variable')
   }
+
   return {
     FIREBASE_CONFIG: {
-      apiKey: ENV[`${QENV}_API_KEY`],
-      authDomain: ENV[`${QENV}_AUTH_DOMAIN`],
-      databaseURL: ENV[`${QENV}_DATA_BASE_URL`],
-      projectId: ENV[`${QENV}_PROJECT_ID`],
-      storageBucket: ENV[`${QENV}_STORAGE_BUCKET`],
-      messagingSenderId: ENV[`${QENV}_MESSAGING_SENDER_ID`],
-      appId: ENV[`${QENV}_APP_ID`]
+      apiKey:  process.env[`${QENV}_API_KEY`],
+      authDomain:  process.env[`${QENV}_AUTH_DOMAIN`],
+      databaseURL:  process.env[`${QENV}_DATA_BASE_URL`],
+      projectId: process.env[`${QENV}_PROJECT_ID`],
+      storageBucket: process.env[`${QENV}_STORAGE_BUCKET`],
+      messagingSenderId:  process.env[`${QENV}_MESSAGING_SENDER_ID`],
+      appId: process.env[`${QENV}_APP_ID`]
     },
-    BLOCKONOMICS_API_KEY: ENV[`${QENV}_BLOCKONOMICS_API_KEY`]
+    BLOCKONOMICS_API_KEY:  process.env[`${QENV}_BLOCKONOMICS_API_KEY`]
   }
 }
